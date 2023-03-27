2899f2c9fb003a470febb97ef4a43545bc6fcdd7
MathiX94 <mathieu.brandao94@gmail.com>
2023-03-27 15:56:31 +0200

    character movement

---
+++ Assets/Scripts/characterMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterMovement : MonoBehaviour
+{
+    public float speed;
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+
+        Vector3 movementDirection = new Vector3(horizontal, jump, vertical);
+        movementDirection *= speed;
+        characterController.Move(movementDirection * Time.deltaTime);
+    }
+}
